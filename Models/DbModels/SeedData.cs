28baf1433c75b647a9e0b353cc2780708a7578ed
Raphael Jambalos <raphael.jambalos@ecloudvalley.com>
2025-02-18 17:53:47 +0800

    Add changes

--- Models/DbModels/SeedData.cs
+++ Models/DbModels/SeedData.cs
@@ -60,6 +60,14 @@ namespace WorkshopApp.Models {
                 context.Challenges.AddRange(c1,c2, c3, c4);
                 context.SaveChanges();
 
+                Hint h2_0 = new Hint {
+                    Name = "Hint 2.0: Creating DynamoDB using AWS Console",
+                    Body = "Visit the <a href='https://walnut-raccoon-d7a.notion.site/Hint-2-1-1pt-Creating-DynamoDB-using-AWS-Console-1929d79bc5b580fcbfccc2d70945418c?pvs=4'> hint here </a>",
+                    Challenge = c2,
+                    Workshop = w1,
+                    Price = 1
+                };
+
                 Hint h2_1 = new Hint {
                     Name = "Hint 2.1: Accessing the request_body and the {product_id} in the url",
                     Body = "Visit the <a href='https://walnut-raccoon-d7a.notion.site/Hint-2-1-18f9d79bc5b580c89563d1315d22ba69?pvs=4'> hint here </a>",
@@ -81,15 +89,28 @@ namespace WorkshopApp.Models {
                     Body = "Visit the <a href='https://walnut-raccoon-d7a.notion.site/Hint-2-3-18f9d79bc5b580dbb53ff632dab6a689?pvs=4'> hint here </a>",
                     Challenge = c2,
                     Workshop = w1,
+                    Price = 3
+                };
+
+                Hint h3_1 = new Hint {
+                    Name = "Hint 3.1: Link AWS Lambda function to the s3 bucket ",
+                    Body = "Visit the <a href='https://www.serverless.com/framework/docs/providers/aws/events/s3#using-existing-buckets'> hint here </a>",
+                    Challenge = c3,
+                    Workshop = w1,
                     Price = 1
                 };
 
-                context.Hints.AddRange(h2_1, h2_2, h2_3);
+                Hint h3_2 = new Hint {
+                    Name = "Hint 3.2: Reading a CSV file from an AWS S3 Bucket",
+                    Body = "Visit the <a href='https://walnut-raccoon-d7a.notion.site/Hint-3-2-5pts-Reading-a-CSV-file-from-an-AWS-S3-Bucket-1929d79bc5b580ed9567d242cc805171?pvs=74s'> hint here </a>",
+                    Challenge = c3,
+                    Workshop = w1,
+                    Price = 3
+                };
+
+                context.Hints.AddRange(h2_0, h2_1, h2_2, h2_3, h3_1, h3_2);
                 context.SaveChanges();
             }
-
-
-
         }
     } 
 }
